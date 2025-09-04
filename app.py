d87b980ab604c6bda81e5b8397de1b0e3e7e0202
nickeldred <nickeldred@gmail.com>
2025-09-03 23:00:18 -0500

    Update app.py [skip_ci]

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 DB_HOST = os.environ.get("DB_HOST")
 DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
-DB_NAME = os.environ.get("DB_NAME", "farmdata")
+DB_NAME = os.environ.get("DB_NAME", "farm")
 
 def get_conn():
     return pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PASS,
